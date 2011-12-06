466dc00e3d1aa676399519694658d960d70da7a8
Manpreet Minhas <mminhas23@gmail.com>
2011-12-06 15:19:46 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scratchpad::Application.config.secret_token = '194b24ca31442723e79c935021c0d507f4fcedbd582331111b8a711dfc38eb028a0b346bd5e19452ca4849a0a4ec94dabdf6496a1fafbff2b5ef7049180a91cd'
